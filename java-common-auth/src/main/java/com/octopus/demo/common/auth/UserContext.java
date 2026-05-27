ebab10f501a35167a463f31c5e0f527e1b3b995a
xiezhifeng <vhp7956@dingtalk.com>
2026-05-27 13:54:50 +0800

    feat: add UserContext ThreadLocal wrapper for userId

---
+++ java-common-auth/src/main/java/com/octopus/demo/common/auth/UserContext.java
@@ -0,0 +1,24 @@
+package com.octopus.demo.common.auth;
+
+/**
+ * ThreadLocal wrapper for current user identity.
+ * Set by AuthInterceptor, cleared in afterCompletion to prevent thread-pool reuse leaks.
+ */
+public final class UserContext {
+
+    private static final ThreadLocal<Long> USER_ID = new ThreadLocal<>();
+
+    private UserContext() {}
+
+    public static void setUserId(Long userId) {
+        USER_ID.set(userId);
+    }
+
+    public static Long getUserId() {
+        return USER_ID.get();
+    }
+
+    public static void clear() {
+        USER_ID.remove();
+    }
+}
